d6f466feda9bc9779c83c161ae845309d6fea56d
educalo77 <educalo@outlook.com>
2021-01-21 20:50:20 -0300

    ok

--- src/db.js
+++ src/db.js
@@ -43,21 +43,21 @@ User.hasOne(Balance);
 Balance.belongsTo(User);
 
 
-const devConfig = {
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE
-}
+// const devConfig = {
+//     user: process.env.PG_USER,
+//     password: process.env.PG_PASSWORD,
+//     host: process.env.PG_HOST,
+//     port: process.env.PG_PORT,
+//     database: process.env.PG_DATABASE
+// }
 //const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL; //heroku addons
+// const proConfig = process.env.DATABASE_URL; //heroku addons
 
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : devConfig,
-});
+// const pool = new Pool({
+//   connectionString:
+//     process.env.NODE_ENV === "production" ? proConfig : devConfig,
+// });
 
 
 
@@ -65,6 +65,5 @@ module.exports = {
     conn: sequelize,
     User,
     Transaction,
-    Balance,
-    pool
+    Balance
 };
