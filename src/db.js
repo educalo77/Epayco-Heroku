47abd9342b8ce8f403b067a9271ac26c8c6afdb5
educalo77 <educalo@outlook.com>
2021-01-21 19:50:57 -0300

    ok

--- src/db.js
+++ src/db.js
@@ -6,18 +6,18 @@ const UserModel = require("./models/user");
 const TransactionModel = require("./models/transaction");
 const BalanceModel = require("./models/balance");
 
-const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
-    host: process.env.PG_HOST,
-    dialect: 'postgres',
-    operatorsAliases: false,
+// const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
+//     host: process.env.PG_HOST,
+//     dialect: 'postgres',
+//     operatorsAliases: false,
    
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-});
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+// });
   
 const sequelize = new Sequelize("postgres://mlpizxglutumwp:9ef203d52ac90ebb47805606e26c32494e19f16ef3507828936d96659362f949@ec2-3-211-149-196.compute-1.amazonaws.com:5432/dtagarr2ddp35", "mlpizxglutumwp", "9ef203d52ac90ebb47805606e26c32494e19f16ef3507828936d96659362f949", {
     host: "ec2-3-211-149-196.compute-1.amazonaws.com",
