18fbfa5076c5c0931dd0668382db49d86bc0257c
educalo77 <educalo@outlook.com>
2021-01-21 22:44:42 -0300

    ok

--- models/user.js
+++ models/user.js
@@ -11,9 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.belongsToMany(models.Transactions, {
-      foreignKey: 'userId',
-      })
+      
+      User.hasMany(models.Transactions, { as: 'transactions' })
+      
       User.hasOne(models.Balance, {
       foreignKey: 'userId',
     })
