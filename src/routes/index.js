a2abe88f83d820904e1fbecc4887c3b55ffd4a5a
educalo77 <educalo@outlook.com>
2021-01-21 22:20:25 -0300

    ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,10 @@ const UserRoutes = require("./userRoutes");
 const TransactionRoutes = require("./transactionRoutes");
 const BalanceRoutes = require("./balanceRoutes");
 const AuthRoutes = require("./authRoutes");
-const index = require("./soapRoutes/index.js");
 
 router.use("/users", UserRoutes);
 router.use("/auth",AuthRoutes);
 router.use("/transactions",TransactionRoutes);
 router.use("/balance",BalanceRoutes);
-router.use("/soap",index);
 
 module.exports = router;
